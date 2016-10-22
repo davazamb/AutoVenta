45a703cb9be3eab1668b7a0707c6390ebc7cc692
David Zambrano <davazamb10@outlook.com>
2016-10-22 19:05:48 -0400

    Init. Created Models; Auto, DbContext. Update Nuget  all reference.

---
+++ AutoVenta/Models/AutoVentaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutoVenta.Models
+{
+    public class AutoVentaContext : DbContext
+    {
+
+    }
+}
---
+++ AutoVenta/Models/Vehiculos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoVenta.Models
+{
+    public class Vehiculos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Year { get; set; }
+
+    }
+}
