95931e4481e7cd11ac1d43ff5fed49d84b6c4756
David Zambrano <davazamb10@outlook.com>
2016-10-22 19:47:53 -0400

    Install toastr, created js app. service and bundle js

--- AutoVenta/App_Start/BundleConfig.cs
+++ AutoVenta/App_Start/BundleConfig.cs
@@ -23,9 +23,15 @@ namespace AutoVenta
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                      "~/Scripts/angular.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/toastr").Include(
+                      "~/Content/toastr.css"));
         }
     }
 }
